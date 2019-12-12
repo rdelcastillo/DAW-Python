2e71382227474e7011587c399fec98e9f6fe29ce
Rafael del Castillo <rafa.castillo@gmail.com>
2019-12-12 20:17:41 +0100

    Ejemplos de uso de librería de funciones

---
+++ 00ejemplos/test_misfunciones_con_prueba.py
@@ -0,0 +1,8 @@
+"""
+Test de las funciones de misfunciones_con_prueba.py
+"""
+import paquete_ejemplo.misfunciones_con_prueba as misf
+
+print("Mi 'namespace' es:", __name__)
+misf.funcion1()
+misf.funcion2("TEST")
