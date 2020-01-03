728e1fbebf28257f9cabab8fc5bc360ee36d5ce9
Rafael del Castillo <rafa.castillo@gmail.com>
2020-01-03 23:52:31 +0100

    Clase Dado versión 2.0

--- ej05_POO/juegos/dado.py
+++ ej05_POO/juegos/dado.py
@@ -4,6 +4,7 @@ Ejemplo de POO. Clase Dado versión 2.
 Cambios:
 * En vez de pasar una lista al dado le pasamos los valores de las caras como parámetros.
 * Ocultamos el atributo "cara" del dado y accedemos a él a través de una propiedad para que no pueda ser cambiado.
+* Creamos una propiedad para que devuelva los valores de las caras.
 
 Autor: Rafael del Castillo.
 """
@@ -30,6 +31,14 @@ class Dado:
         """
         return self.__cara
 
+    @property
+    def caras(self):
+        """
+        Similar getter de Java, pero con una filosofía 'pythonica'.
+        En Python se prefiere acceder a los atributos directamente, si son ocultos se hace con una propiedad.
+        """
+        return self.__caras
+
     def tirada(self):
         """
         Simula la tirada del dado.
@@ -41,6 +50,7 @@ class Dado:
 
 if __name__ == "__main__":
     d = Dado(1,2,3,4,5,6)
+    print("Caras del dado:", d.caras)
     print("Tiramos dado 5 veces: ", end="")
     for i in range(5):
         d.tirada()
