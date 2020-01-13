8c160e8f82baaa38b5953c0763e3aa612dec31f6
Rafael del Castillo <rafa.castillo@gmail.com>
2020-01-13 11:54:13 +0100

    Clase Rectángulo versión 2.0

--- ej05_POO/rectangulo.py
+++ ej05_POO/rectangulo.py
@@ -1,32 +1,84 @@
 class Rectangulo:
     """
-    Versión 1.0.
+    Versión 2.0.
 
     Esta clase representa objetos de tipo rectángulo.
     Acciones: cálculo del perímetro, área, dibujar, comparar.
 
+    Mejoras respecto a la versión 1:
+
+    - Protegemos (ocultamos) los atributos.
+    - Creamos getters y setters para acceder a los mismos. Ojo, esta no
+    es la filosofía de Python, esto es estilo Java.
+    - Sobrecargamos el método __str__ para poder usar la función print y otras.
+
     Hay cosas que mejorar, en la siguiente se explica.
     """
+
     def __init__(self, base, altura):
         """
         Constructor de la clase.
         :param base: base del rectángulo.
         :param altura: altura del rectángulo.
         """
-        self.base = base
-        self.altura = altura
+        self.__base = 1
+        self.__altura = 1
+        # Ojo, solo así en este ejemplo
+        self.set_base(base)
+        self.set_altura(altura)
+
+    # getters y setters (ojo!!! estilo Java, no Python)
+
+    def get_base(self):
+        """
+        Getter tipo Java
+        :return: base
+        """
+        return self.__base
+
+    def set_base(self, base):
+        """
+        Setter tipo Java.
+        :param base:
+        """
+        if base>0:
+            self.__base = base
+        else:
+            # Mucho mejor sería lanzar una excepción
+            print("ERROR. Base incorrecta")
+
+    def get_altura(self):
+        """
+        Getter tipo Java
+        :return:
+        """
+        return self.__altura
+
+    def set_altura(self, altura):
+        """
+        Setter tipo Java
+        :param altura:
+        :return: altura
+        """
+        if altura>0:
+            self.__altura = altura
+        else:
+            # Mucho mejor sería lanzar una excepción
+            print("ERROR. Altura incorrecta")
+
+    # resto métodos
 
     def perimetro(self):
         """
         :return: perímetro del rectángulo.
         """
-        return 2 * (self.base + self.altura)
+        return 2 * (self.__base + self.__altura)
 
     def area(self):
         """
         :return: área del rectángulo.
         """
-        return self.base * self.altura
+        return self.__base * self.__altura
 
     def compara(self, otro):
         """
@@ -43,20 +95,31 @@ class Rectangulo:
         :param otro: objeto con el que comparamos el actual.
         :return: True o False
         """
-        return self.base==otro.base and self.altura==otro.altura
+        return self.__base == otro.get_base() and self.__altura == otro.get_altura()
 
     def muestra(self):
         """
         Muestra en pantalla el rectángulo.
         """
-        for i in range(self.altura):
-            print("*" * self.base)
+        print(self)
 
-if __name__  == "__main__":
+    # métodos sobrecargados
+
+    def __str__(self):
+        str = ""
+        for i in range(self.__altura):
+            str += "*" * self.__base
+            str += "\n"
+        str = str[:-1]
+        return str
+
+
+
+if __name__ == "__main__":
     r1 = Rectangulo(4, 1)
     r2 = Rectangulo(3, 2)
-    print(f"Probamos clase rectángulo con r1: ({r1.base},{r1.altura}) y "
-          f"r2: ({r2.base},{r2.altura})\n")
+    print(f"Probamos clase rectángulo con r1: ({r1.get_base()},{r1.get_altura()}) y "
+          f"r2: ({r2.get_base()},{r2.get_altura()})\n")
     # mostramos
     r1.muestra()
     print()
@@ -67,4 +130,3 @@ if __name__  == "__main__":
     # magnitudes
     print("Área r1:", r1.area(), "Perímetro r1:", r1.perimetro())
     print("Área r2:", r2.area(), "Perímetro r2:", r2.perimetro())
-
