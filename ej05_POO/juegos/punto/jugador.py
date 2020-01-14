34562e646f2b0a24b4563155893db3d6c2fb907e
Rafael del Castillo <rafa.castillo@gmail.com>
2020-01-14 13:16:48 +0100

    Juego de dados 'Punto'

--- ej05_POO/juegos/punto/jugador.py
+++ ej05_POO/juegos/punto/jugador.py
@@ -1,17 +1,42 @@
+"""
+Clases para jugadores del juego de dados del Punto:
+
+* Jugador: jugador normal.
+* Jugador tramposo.
+
+Se hace uso de las clases para los dados ya implementados, si la variable de entorno PYTHONPATH
+no está bien puesta no funcionará.
+"""
+
 from dados import DadoSimple
+from dado import Dado
 
 
-class JugadorPunto:
+class Jugador:
     """
     Clase que implementa un jugador para el juego del punto:
     http://www.acanomas.com/Reglamentos-Juegos-de-Dados/042/Punto.htm
+
+    Variables de instancia:
+        * __nombre: nombre del jugador.
+        * __dados: tupla con tres objetos de tipo DadoSimple.
+        * __puntos: puntuación acumulada.
+        * __puntos_tirada: puntuación tirada actual.
+        * __tiro_premiado: 0 si no hay premio y valor del dado repetido (2, 4 ó 6) si lo hay.
+
+    Métodos:
+        * tirar.
+        * __tira_dados (el método anterior hace uso de este).
+        * inicializa_marcador
     """
-    __puntuaciones = (1, 0, 2, 0, 4, 0) # puntuación de cada valor del dado
+    __puntuaciones = (1, 0, 2, 0, 4, 0)  # puntuación de cada valor del dado
 
     def __init__(self, nombre):
         self.__nombre = nombre
-        self.__dados = (DadoSimple(), DadoSimple(), DadoSimple())   # necesita tres dados
+        self.__dados = (DadoSimple(), DadoSimple(), DadoSimple())  # necesita tres dados
         self.__puntos = 0
+        self.__puntos_tirada = 0
+        self.__tiro_premiado = 0  # si es tiro premiado guardaremos 2, 4 ó 6 (dado repetido)
 
     @property
     def nombre(self):
@@ -22,17 +47,95 @@ class JugadorPunto:
         return self.__dados
 
     @property
+    def tiro_premiado(self):
+        return self.__tiro_premiado
+
+    @property
     def puntos(self):
         return self.__puntos
 
-    def tirada(self):
+    @property
+    def puntos_tirada(self):
+        return self.__puntos_tirada
+
+    def haz_tirada(self):
         """
         Tira los tres dados y acumula la puntuación.
         :return: puntuación obtenida
         """
+        self.__tiro_premiado = 0    # si había premio en la tirada anterior se anula
+        p = self.__tira_dados()     # tiramos los dados y recogemos puntuación
+        # ¿es tiro premiado?
+        d1, d2, d3 = self.dados[0].cara, self.dados[1].cara, self.dados[2].cara
+        if (d1 == d2 == d3) and d1 in (2, 4, 6):    # si se cumple es un tiro premiado
+            self.__tiro_premiado = d1
+            p = 2 * self.__tira_dados()             # tiramos de nuevo y puntuamos doble
+        # acumulamos puntuación y devolvemos la obtenida en la tirada
+        self.__puntos += p
+        self.__puntos_tirada = p
+        return p
+
+    def __tira_dados(self):
+        """
+        Tira los dados del jugador.
+        :return: puntuación de la tirada
+        """
         p = 0
         for d in self.dados:
             d.tirada()
-            p += JugadorPunto.__puntuaciones[d.cara - 1]
-        self.__puntos += p
+            p += Jugador.__puntuaciones[d.cara - 1]
         return p
+
+    def inicializa_marcador(self):
+        self.__puntos = 0
+
+
+class JugadorTramposo(Jugador):
+    """
+    Jugador del juego del punto que usa dados trucados.
+    """
+    __valores_dado = (1, 1, 2, 3, 3, 3, 4, 5, 5, 5, 5)  # más probabilidad para 5, 3 y 1
+
+    def __init__(self, nombre):
+        """
+        Constructor de JugadorTramposo.
+        Creamos nuestros propios dados, que serán trucados.
+        :param nombre: nombre del jugador.
+        """
+        super().__init__(nombre)
+        caras = JugadorTramposo.__valores_dado
+        self.__dados_trucados = (Dado(*caras), Dado(*caras), Dado(*caras))
+
+    @property
+    def dados(self):
+        """
+        Redefinimos la propiedad de la clase de la que hereda para que devuelva los dados trucados.
+        :return: dados trucados.
+        """
+        return self.__dados_trucados
+
+
+if __name__ == "__main__":
+    j1 = Jugador("Honrado")
+    j2 = JugadorTramposo("Tramposo")
+    # Tiramos cuatro rondas
+    for i in range(4):
+        print("RONDA", i + 1)
+        print(".......")
+        for j in j1, j2:
+            print(f"Tirada {j.nombre}: {j.haz_tirada()} puntos.")
+            if j.tiro_premiado:
+                print("Este tirada tiene PREMIO:", j.tiro_premiado)
+            print("Dados: ", end="")
+            for d in j.dados:
+                print(d.cara, end=" ")
+            print()
+            print(f"Puntuación acumulada: {j.puntos}")
+            print("---")
+        print("")
+    # Ganador
+    if j1.puntos > j2.puntos:
+        g = j1
+    else:
+        g = j2
+    print("GANADOR:", g.nombre)
---
+++ ej05_POO/juegos/punto/partidapunto.py
@@ -0,0 +1,161 @@
+"""
+Clase para jugar al juego de dados del Punto:
+
+Se hace uso de las clases para los jugadores ya implementados, entre ellas un "jugador tramposo".
+Si la variable de entorno PYTHONPATH no está bien puesta no funcionará.
+"""
+
+from jugador import Jugador, JugadorTramposo
+
+
+class PartidaPunto:
+    """
+    Clase que implementa una partida para el juego del punto:
+    http://www.acanomas.com/Reglamentos-Juegos-de-Dados/042/Punto.htm
+
+    Variables de instancia:
+        * __jugadores: tupla con objetos de clase Jugador.
+        * __turno: turno de la partida (hasta 4).
+        * __pausa_en_tirada: Verdadero si queremos que se haga una pausa antes de tirar los dados.
+        * __salida: lo que devolverá __str__() que será lo que salga en pantalla.
+
+    Métodos:
+        * juega: ejecuta la partida.
+        * juega_turno: ejecuta uno de los turnos.
+        * juega_turno_jugador: ejecuta la jugada de un jugador en un turno.
+        * salida_tirada: lo que debería salir por pantalla en una tirada. Método estático.
+        * __pon_en_pantalla: manda a la salida (pantalla) el texto pasado como parámetro.
+        * marcador: devuelve el "marcador" final.
+        * __puntuacion: devuelve la puntuación de un jugador. Método estático.
+    """
+    __TURNOS = 4
+
+    def __init__(self, *jugadores):
+        """
+        Constructor de la clase.
+        :param jugadores: lista de objetos de clase Jugador
+        """
+        self.__jugadores = jugadores
+        self.__turno = 0
+        self.__pausa_en_tirada = False
+        self.__salida = [""] * (PartidaPunto.__TURNOS + 1)
+
+    # propiedades get/set
+    @property
+    def jugadores(self):
+        return self.__jugadores
+
+    @property
+    def turno(self):
+        return self.__turno
+
+    @property
+    def pausa_en_tirada(self):
+        return self.__pausa_en_tirada
+
+    @pausa_en_tirada.setter
+    def pausa_en_tirada(self, value):
+        self.__pausa_en_tirada = value
+
+    # métodos
+    def juega(self):
+        """
+        Ejecuta la partida.
+        :return: salida en pantalla.
+        """
+        # inicio
+        self.__pon_en_pantalla("JUEGO DEL PUNTO")
+        self.__pon_en_pantalla("---------------\n")
+        # turnos
+        for t in range(PartidaPunto.__TURNOS):
+            self.juega_turno()
+        # marcador final
+        self.__pon_en_pantalla(self.marcador())
+        return self.__str__()
+
+    def juega_turno(self):
+        """
+        Juega un turno de la partida.
+        """
+        self.__turno += 1
+        self.__pon_en_pantalla(f"Turno {self.turno}")
+        self.__pon_en_pantalla("-------")
+        for j in self.jugadores:
+            self.juega_turno_jugador(j)
+        self.__pon_en_pantalla("")
+
+    def juega_turno_jugador(self, j):
+        """
+        Juega un turno de un jugador.
+        :param j: jugador que juega el turno.
+        """
+        if self.pausa_en_tirada:
+            print(f"{j.nombre} pulsa Intro para tirar los dados.", end="")
+            input()
+        j.haz_tirada()
+        self.__pon_en_pantalla(PartidaPunto.salida_tirada(j))
+
+    @staticmethod
+    def salida_tirada(j):
+        """
+        Crea la salida por pantalla del jugador que se pasa como parámetro.
+        :param j: jugador.
+        :return: cadena que debe mostrarse por la pantalla.
+        """
+        salida = f"Dados {j.nombre}: "
+        for d in j.dados:
+            salida += f"{d.cara} "
+        salida += f"\tPuntos: {j.puntos_tirada}\tACUMULADO: {j.puntos}"
+        if j.tiro_premiado:
+            salida += f"\tPREMIADO ({j.tiro_premiado})"
+        return salida
+
+    def __pon_en_pantalla(self, salida):
+        """
+        Manda a la "salida" la cadena enviada como parámetro. Si está activa la pausa en la tirada de dados
+        hace una parada antes de la tirada de dados.
+        :param salida: lo que debe mandarse a la "salida"
+        """
+        self.__salida[self.turno] += salida + "\n"
+        if self.pausa_en_tirada:
+            print(salida)
+
+    def marcador(self):
+        """
+        Crea la salida por pantalla del marcador final.
+        :return: cadena con el marcador que saldrá por la pantalla.
+        """
+        puesto = 1
+        salida = ""
+        l = list(self.jugadores)
+        # ordenamos jugadores por puntuación
+        l.sort(reverse=True, key=PartidaPunto.__puntuacion)
+        # marcador
+        self.__pon_en_pantalla("MARCADOR FINAL")
+        self.__pon_en_pantalla("--------------")
+        for j in l:
+            salida += f"Puesto: {puesto}. {j.nombre}: {j.puntos} PUNTOS\n"
+        salida += f"\nGANA: {l[0].nombre}"
+        return salida
+
+    @staticmethod
+    def __puntuacion(j):
+        """
+        Devuelve la puntuación acumulada de un jugador. Necesario para ordenar.
+        :param j: jugador.
+        :return: puntuación.
+        """
+        return j.puntos
+
+    def __str__(self):
+        salida = ""
+        for s in self.__salida:
+            salida += s
+        return salida
+
+
+if __name__ == '__main__':
+    p = PartidaPunto(Jugador("Jugador1"), JugadorTramposo("Tramposo"), Jugador("Jugador2"))
+    # p.pausa_en_tirada = True
+    print(p.juega())
+    # p.juega(); print(p)
