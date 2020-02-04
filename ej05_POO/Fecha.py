3c694d5c32abec557da3851463e71c71853636ec
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2020-02-04 10:22:54 +0100

    Clase Fecha con sobrecarga operadores aritméticos

--- ej05_POO/Fecha.py
+++ ej05_POO/Fecha.py
@@ -40,10 +40,65 @@ class Fecha:
         assert Fecha.es_correcta(self.__dia, self.__mes, value)
         self.__anyo = value
 
+    # Métodos
+
+    def __suma_1dia(self):
+        """
+        :return: Fecha almacenada + 1 día
+        """
+        dia = self.__dia + 1
+        mes = self.__mes
+        anyo = self.__anyo
+        if dia > Fecha.dias_mes(mes, anyo):
+            # mes siguiente
+            dia = 1
+            mes += 1
+            if mes > 12:  # nos pasamos de diciembre, año siguiente
+                mes = 1
+                anyo += 1
+        return Fecha(dia, mes, anyo)
+
+    def __resta_1dia(self):
+        """
+        :return: Fecha almacenada - 1 día
+        """
+        dia = self.__dia - 1
+        mes = self.__mes
+        anyo = self.__anyo
+        if dia == 0:
+            # mes anterior
+            mes -= 1
+            if mes == 0:  # nos vamos al año anterior
+                mes = 12
+                anyo -= 1
+            dia = Fecha.dias_mes(mes, anyo)
+        return Fecha(dia, mes, anyo)
+
+    def nombre_mes(self):
+        meses = ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+                 "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+        return meses[self.mes-1]
+
     # Sobrecarga
 
     def __str__(self):
-        pass
+        return f"{self.__dia} de {self.nombre_mes()} de {self.__anyo}"
+
+    def __add__(self, n):
+        f = self
+        if n > 0:
+            for i in range(n):
+                f = f.__suma_1dia()
+        else:
+            for i in range(abs(n)):
+                f = f.__resta_1dia()
+        return f
+
+    def __sub__(self, n):
+        return self + -1*n
+
+    def __radd__(self, n):
+        return self + n     # también vale "return self.__add__(n)"
 
     # Métodos estáticos
 
@@ -63,7 +118,14 @@ class Fecha:
         if mes < 1 or mes > 12:
             return False
         # día correcto
-        dias_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        return 0 < dia <= Fecha.dias_mes(mes, anyo)
+
+    @staticmethod
+    def dias_mes(mes, anyo):
+        """
+        :return: Días del mes actual
+        """
+        dias_mes_ = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         if Fecha.es_bisiesto(anyo):
-            dias_mes[1] = 29
-        return 0 < dia <= dias_mes[mes - 1]
+            dias_mes_[1] = 29
+        return dias_mes_[mes - 1]
