b831b90f417384bc2d0ecc4e7aa2ed7b7dcf6567
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2020-03-03 10:08:55 +0100

    Pronóstico temperatura próximos 5 días V2.0

--- ej06_REST-API/tiempo_en_5_dias.py
+++ ej06_REST-API/tiempo_en_5_dias.py
@@ -3,6 +3,10 @@ Usando la API de OpenWeather obtendremos el pronóstico del tiempo para una ciud
 próximos cinco días, mostraremos:
 
 Temperatura media, mínima y máxima (en grados Celsius) para cada día y global.
+
+Para las temperaturas medias usaremos la clave "temp" del json devuelto por la API.
+Para las temperaturas mínimas usaremos la clave "temp_min" del json devuelto por la API.
+Para las temperaturas máximas usaremos la clave "temp_max" del json devuelto por la API.
 """
 import requests
 import os
@@ -12,7 +16,7 @@ ciudad = input("Ciudad de la que quiere conocer el pronóstico del tiempo en los
 
 # datos para hacer la petición
 url = "https://api.openweathermap.org/data/2.5/forecast"
-params = {"q":ciudad, "appid":os.environ["OPEN_WEATHER_KEY"], "units":"metric", "lang":"es"}
+params = {"q": ciudad, "appid": os.environ["OPEN_WEATHER_KEY"], "units": "metric", "lang": "es"}
 
 # petición
 response = requests.get(url, params=params)
@@ -22,24 +26,43 @@ if response.status_code != 200:
 datos = response.json()
 
 # cálculos
-dias = dict()       # diccionario con clave el día y valor la lista de mediciones del día
-totales = list()    # lista de mediciones de todos los días
+dias = dict()  # diccionario con clave el día y valor la lista de mediciones del día
+totales = {"temp": [], "temp_min": [], "temp_max": []}  # lista de mediciones de todos los días
+
 for medicion in datos["list"]:
     # fecha y temperatura de la medición
     dia = medicion["dt_txt"][:10]
     temp = float(medicion["main"]["temp"])
+    temp_min = float(medicion["main"]["temp_min"])
+    temp_max = float(medicion["main"]["temp_max"])
+
     # si no tenemos datos de ese día creamos una nueva entrada en el diccionario
     if not dias.get(dia):
-        dias[dia] = list()
+        dias[dia] = {"temp": [], "temp_min": [], "temp_max": []}
     # añadimos medición
-    dias[dia].append(temp)
-    totales.append(temp)
+    dias[dia]["temp"].append(temp)
+    dias[dia]["temp_min"].append(temp_min)
+    dias[dia]["temp_max"].append(temp_max)
+    totales["temp"].append(temp)
+    totales["temp_min"].append(temp_min)
+    totales["temp_max"].append(temp_max)
 
 # resultados
 print()
-for dia,temps in dias.items():
-    print(f"Día {dia[8:]}-{dia[5:7]}-{dia[0:4]}:\tTemperatura media: {(sum(temps)/len(temps)):.2f}º, "
-          f"mínima: {min(temps)}º y máxima: {max(temps)}º")
+for dia, temps in dias.items():
+    # diario
+    temp_med = sum(temps['temp']) / len(temps['temp'])
+    temp_min = min(temps['temp_min'])
+    temp_max = max(temps['temp_max'])
+    print(f"Día {dia[8:]}-{dia[5:7]}-{dia[0:4]}:\t"
+          f"Temperatura media: {temp_med:.2f}º, "
+          f"mínima: {temp_min}º y máxima: {temp_max}º. "
+          f"Mediciones: {len(temps['temp'])}")
 print()
-print(f"TOTALES:\t\tTemperatura media: {(sum(totales)/len(totales)):.2f}º, "
-      f"mínima: {min(totales)}º y máxima: {max(totales)}º")
+
+# global
+temp_med = sum(totales['temp']) / len(totales['temp'])
+temp_min = min(totales['temp_min'])
+temp_max = max(totales['temp_max'])
+print(f"TOTALES:\t\tTemperatura media: {temp_med:.2f}º, "
+      f"mínima: {temp_min}º y máxima: {temp_max}º")
