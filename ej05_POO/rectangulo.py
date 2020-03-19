bf8b4516129c6a71522eecac7c54b6734dafe938
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2020-03-19 17:24:15 +0100

    Versión 5.0 de la clase Rectángulo

--- ej05_POO/rectangulo.py
+++ ej05_POO/rectangulo.py
@@ -1,15 +1,29 @@
+import pickle
+import sys
+
+class DimensionRectanguloError(Exception):
+    """
+    Excepción que se produce cuando no se respetan las dimensiones permitidas de un rectángulo.
+    """
+    def __init__(self, mensaje):
+        super.__init__(self)
+        self.mensaje_error = mensaje
+
+
 class Rectangulo:
     """
-    Versión 4.0.
+    Versión 5.0.
 
     Esta clase representa objetos de tipo rectángulo.
     Acciones: cálculo del perímetro, área, dibujar, comparar.
 
-    Mejoras respecto a la versión 3.01:
+    Mejoras respecto a la versión 4.0:
 
-    - Añadimos destructor.
-    - Ponemos assert.
-    - Sobrecargamos operador *.
+    - Sustituimos los assert por excepciones:
+        * cuando se le asigne al rectángulo un valor que no sea entero en el lado o en el ancho tiene que lanzar la excepción "TypeError".
+        * crea una excepción propia para cuando el rectángulo sobrepasa las dimensiones permitidas: "DimensionRectanguloError".
+    - Tiene un método (guardar) para guardar en un fichero el objeto actual usando pickle.
+    - Tiene un método (recuperar) para recuperar un objeto Rectángulo almacenado en un fichero usando pickle.
     """
     lado_maximo = 10  # lado máximo del rectángulo
     __num_creados = 0  # contador de rectángulos creados
@@ -38,7 +52,7 @@ class Rectangulo:
 
     @base.setter
     def base(self, value):
-        assert self.es_lado_correcto(value)
+        Rectangulo.__comprueba_lado(value)
         self.__base = value
 
     @property
@@ -47,7 +61,7 @@ class Rectangulo:
 
     @altura.setter
     def altura(self, value):
-        assert Rectangulo.es_lado_correcto(value)
+        Rectangulo.__comprueba_lado(value)
         self.__altura = value
 
     # resto métodos
@@ -58,7 +72,20 @@ class Rectangulo:
 
     @staticmethod
     def es_lado_correcto(value):
-        return type(value) == type(1) and 0 < value <= Rectangulo.lado_maximo
+        return isinstance(value, int) and 0 < value <= Rectangulo.lado_maximo
+
+    @staticmethod
+    def __comprueba_lado(value):
+        """
+        Comprueba el valor recibido, si no es entero lanza la excepción TypeError y si no está dentro de los límites
+        lanza la excepción DimensionRectanguloError.
+
+        :param value: base o una altura del rectángulo.
+        """
+        if not isinstance(value, int):
+            raise TypeError
+        elif value < 0 or value > Rectangulo.lado_maximo:
+            raise DimensionRectanguloError(f"{value} no está dentro de las dimensiones permitidas.")
 
     def perimetro(self):
         """
@@ -95,6 +122,40 @@ class Rectangulo:
         """
         print(self)
 
+    def guarda(self, fichero):
+        """
+        Guarda el objeto actual en el fichero enviado como parámetro.
+        :param fichero: nombre del fichero que hay que guardar.
+        :return: true si se ha podido hacer y false si no se ha podido hacer.
+        """
+        try:
+            f = open(fichero, "wb")
+            pickle.dump(self, f)
+            f.close()
+            return True
+        except:
+            print("No se ha podido crear el fichero.", fichero, file=sys.stderr)
+            return False
+
+    def recupera(self, fichero):
+        """
+        Recupera un objeto rectángulo guardado en un fichero con el método 'guarda' en el objeto actual.
+        :param fichero: nombre del fichero donde se guardó el objeto.
+        :return: true si se ha podido hacer y false si no se ha podido hacer.
+        """
+        try:
+            # recuperamos objeto del fichero
+            f = open(fichero, "rb")
+            r = pickle.load(f)    # objeto Rectángulo auxiliar
+            f.close()
+            # asignamos el estado al objeto actual
+            self.base = r.base
+            self.altura = r.altura
+            return True
+        except:
+            print("No se ha podido recuperar el objeto.", fichero, file=sys.stderr)
+            return False
+
     # métodos sobrecargados
 
     def __str__(self):
@@ -107,13 +168,17 @@ class Rectangulo:
 
     def __mul__(self, other):
         """
-        Multiplica la base si no se pasa del lado máximo, en ese caso lo
-        hace con la altura.
+        Multiplica la base por el parámetro si no se pasa del lado máximo, en ese caso lo hace con la altura.
         :param other: Valor entero positivo
         :return: Otro rectángulo con la superficie original*other.
         """
-        assert type(other) == type(1) and other > 0  # operando correcto
-        assert self.base * other <= Rectangulo.lado_maximo or self.altura * other <= Rectangulo.lado_maximo
+        if not isinstance(other, int):  # no es entero el operando, error
+            raise TypeError
+        if other == 0:                  # es cero, error, no hay rectángulos de superficie 0
+            raise DimensionRectanguloError("No puede haber rectángulos de superficie 0")
+        if self.base * other > Rectangulo.lado_maximo and self.altura * other > Rectangulo.lado_maximo:
+            raise DimensionRectanguloError(f"Multiplicar por {other} sobrepasa las dimensiones.")
+        # proceso
         if self.base * other <= Rectangulo.lado_maximo:
             return Rectangulo(self.base * other, self.altura)
         else:
@@ -123,15 +188,18 @@ class Rectangulo:
         return self * other
 
     def __lt__(self, other):
-        assert isinstance(other, Rectangulo)
+        if not isinstance(other, Rectangulo):
+            raise TypeError
         return self.area() < other.area()
 
     def __le__(self, other):
-        assert isinstance(other, Rectangulo)
+        if not isinstance(other, Rectangulo):
+            raise TypeError
         return self.area() <= other.area()
 
     def __eq__(self, other):
-        assert isinstance(other, Rectangulo)
+        if not isinstance(other, Rectangulo):
+            raise TypeError
         return self.area() == other.area()
 
 
