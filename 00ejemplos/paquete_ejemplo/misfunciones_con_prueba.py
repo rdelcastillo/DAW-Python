5672effb6f67c2c0348c943977a5bfe2d3c3cfb6
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2020-03-25 09:02:48 +0100

    Ejemplo de uso de paquetes y librería de funciones

--- 00ejemplos/paquete_ejemplo/misfunciones_con_prueba.py
+++ 00ejemplos/paquete_ejemplo/misfunciones_con_prueba.py
@@ -2,12 +2,15 @@
 Ejemplo de biblioteca de funciones con test dentro del archivo.
 """
 
+
 def funcion1():
     print("Soy la función 1 y mi 'namespace' es:", __name__)
+
 
 def funcion2(param):
     print("Soy la función 2 y me has pasado", param, "como parámetro",
           "y mi 'namespace' es:", __name__)
+
 
 if __name__ == "__main__":
     funcion1()
--- 00ejemplos/paquete_ejemplo/subpaquete_ejemplo/misfunciones2.py
+++ 00ejemplos/paquete_ejemplo/subpaquete_ejemplo/misfunciones2.py
@@ -2,5 +2,6 @@
 Ejemplo funciones dentro de un subpaquete
 """
 
+
 def funcion4(p1="param1", p2="param2"):
     print(f"P1 {p1} y P2 {p2}")
