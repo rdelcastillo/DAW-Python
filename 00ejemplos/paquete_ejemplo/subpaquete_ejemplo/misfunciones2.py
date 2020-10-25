60cac2a71a8733490aeacf9ff9bd1045e891610f
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2020-10-25 20:40:35 +0100

    Ejemplos de clase. 25/10/2020

--- 00ejemplos/paquete_ejemplo/subpaquete_ejemplo/misfunciones2.py
+++
@@ -1,7 +0,0 @@
-"""
-Ejemplo funciones dentro de un subpaquete
-"""
-
-
-def funcion4(p1="param1", p2="param2"):
-    print(f"P1 {p1} y P2 {p2}")
--- 00ejemplos/test_misfunciones_con_prueba2.py
+++
@@ -1,8 +0,0 @@
-"""
-Test de las funciones de misfunciones_con_prueba.py
-"""
-import paquete_ejemplo.misfunciones_con_prueba2 as misf
-
-print("Mi 'namespace' es:", __name__)
-misf.funcion1()
-misf.funcion2("TEST")
