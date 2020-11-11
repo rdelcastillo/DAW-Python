e6e080f8954eb6d120f22b975687956f33ff630f
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2020-11-11 16:56:15 +0100

    Adivinar un número al azar

--- ej02_repetitivas/Ej01Repetitivas.py
+++ ej02_repetitivas/Ej01Repetitivas.py
@@ -1,52 +1,71 @@
-'''
+"""
 Aplicación que permita adivinar un número.
 
-La aplicación genera un número aleatorio del 1 al 100. A continuación va
-pidiendo números y va respondiendo si el número a adivinar es mayor o
-menor que el introducido, además de los intentos que te quedan
-(tienes 10 intentos para acertarlo).
+La aplicación genera un número aleatorio del 1 al 100. A continuación va pidiendo números y va respondiendo
+si el número a adivinar es mayor o menor que el introducido, además de los intentos que te quedan
+(hay 10 intentos para acertarlo).
 
-El programa termina cuando se acierta el número (además te dice en
-cuantos intentos lo has acertado), si se llega al limite de intentos te
-muestra el número que había generado.
+El programa termina cuando se acierta el número (además dice en cuantos intentos se ha acertado),
+si se llega al límite de intentos muestra el número que había generado.
 
-Fecha: 11/11/2019.
+Fecha: 10/11/2020.
 
-Autores: Clase de 1ºDAW
-
+Autores: Clase de 1ºDAW. Idea original de Sergio Vera.
+-------------------------------------------------------------------------------------------------------------
 Variables a usar:
 
-- n_adivinar
-- n
-- intentos
-'''
+- numero_a_adivinar
+- numero_introducido    (número introducido por el usuario)
+- intentos_que_quedan
+
+Algoritmo:
+
+numero_a_adivinar <-- Aleatorio entre 1 y 100
+intentos_que_quedan <-- 10
+
+REPETIR
+    LEER numero_introducido
+    SI numero_introducido < numero_a_adivinar
+        ESCRIBIR numero_introducido, " es menor que el número a adivinar"
+    SINO
+        SI numero_introducido > numero_a_adivinar
+            ESCRIBIR numero_introducido, " es mayor que el número a adivinar"
+        FIN-SI
+    FIN-SI
+    intentos_que_quedan <-- intentos_que_quedan -1
+HASTA QUE intentos_que_quedan=0 O numero_a_adivinar=numero_introducido
+
+SI numero_a_adivinar = numero_introducido
+    ESCRIBIR "Acertaste en ", 10-intentos_que_quedan, "intentos"
+SINO
+    ESCRIBIR "No lo adivinaste, era ", numero_a_adivinar
+FIN-SI
+"""
 
 import random
 
-# Constantes
-INTENTOS_MAXIMOS = 10
-
 # Inicializamos
-intentos = 0
-n_adivinar = random.randrange(1,100)
+numero_a_adivinar = random.randint(1, 100)
+intentos_que_quedan = 10
 
 # Proceso
-while True:  # implementación ciclo postcondición
-    n = int(input(  "Te quedan " + str(INTENTOS_MAXIMOS-intentos) +
-                    " intentos. Introduce un número entre 1 y 100: "))
-    intentos+=1
-    if n < n_adivinar:
-        print(f"{n} es menor que el número a adivinar.")
-    elif n > n_adivinar:
-        print(f"{n} es mayor que el número a adivinar.")
+while True:  # implementación ciclo postcondición REPETIR
+    numero_introducido = int(input(f"Te quedan {intentos_que_quedan} intentos. Introduce un número entre 1 y 100: "))
+
+    # pista (si no acierta) para que le sea más fácil adivinar
+    if numero_introducido < numero_a_adivinar:
+        print(f"{numero_introducido} es menor que el número a adivinar.")
+    elif numero_introducido > numero_a_adivinar:
+        print(f"{numero_introducido} es mayor que el número a adivinar.")
+
+    intentos_que_quedan -= 1    # hemos consumido un intento
+
     # salida ciclo
-    if n==n_adivinar or intentos==INTENTOS_MAXIMOS:
-        # acabo si adivino o supero los intentos
+    if numero_introducido == numero_a_adivinar or intentos_que_quedan == 0:  # acabo si adivino o supero los intentos
         break
 
-# Mostramos resultado
-if n==n_adivinar: # ha adivinado
-    print(f"Has adivinado el número en {intentos} intentos")
+# Mostramos si acertó o no
+if numero_introducido == numero_a_adivinar:  # ha adivinado
+    print(f"Has adivinado el número en {10 - intentos_que_quedan} intentos")
 else:
-    print(f"Has agotado el número máximo de intentos. El número a adivinar era {n_adivinar}")
-
+    print(f"Has agotado el número máximo de intentos. El número a adivinar era {numero_a_adivinar}")
--- ej02_repetitivas/Ej01RepetitivasV2.py
+++ ej02_repetitivas/Ej01RepetitivasV2.py
@@ -1,4 +1,4 @@
-'''
+"""
 Aplicación que permita adivinar un número.
 
 La aplicación genera un número aleatorio del 1 al 100. A continuación va
@@ -14,12 +14,8 @@ Fecha: 11/11/2019.
 
 Autores: Clase de 1ºDAW
 
-Variables a usar:
-
-- num_secreto
-- n
-- intentos_que_quedan
-'''
+Otra versión del programa anterior.
+"""
 
 import random
 
@@ -27,26 +23,25 @@ import random
 INTENTOS_MAXIMOS = 10
 
 # Inicializamos
-num_secreto = random.randrange(1,100)
+numero_a_adivinar = random.randint(1, 100)
 
 # Proceso
 
-# implementamos un ciclo postcondición, ejecutamos la instrución
+# implementamos un ciclo postcondición, ejecutamos la instrucción
 # antes de entrar en el ciclo y al final del ciclo.
-n = int(input("Introduce un número entre 1 y 100: "))
-intentos_que_quedan = INTENTOS_MAXIMOS-1
-while n!=num_secreto and intentos_que_quedan>0:
-    if n < num_secreto:
-        print(f"{n} es menor que el número a adivinar.")
+numero_introducido = int(input("Introduce un número entre 1 y 100: "))
+intentos_que_quedan = INTENTOS_MAXIMOS - 1
+while numero_introducido != numero_a_adivinar and intentos_que_quedan > 0:
+    if numero_introducido < numero_a_adivinar:
+        print(f"{numero_introducido} es menor que el número a adivinar.")
     else:
-        print(f"{n} es mayor que el número a adivinar.")
-    n = int(input(  "Te quedan " + str(intentos_que_quedan) +
-                    " intentos. Introduce un número entre 1 y 100: "))
+        print(f"{numero_introducido} es mayor que el número a adivinar.")
+    numero_introducido = int(input("Te quedan " + str(intentos_que_quedan) +
+                                   " intentos. Introduce un número entre 1 y 100: "))
     intentos_que_quedan -= 1
 
 # Mostramos resultado
-if n==num_secreto: # ha adivinado
-    print(f"Has adivinado el número en {INTENTOS_MAXIMOS-intentos_que_quedan} intentos")
+if numero_introducido == numero_a_adivinar:  # ha adivinado
+    print(f"Has adivinado el número en {INTENTOS_MAXIMOS - intentos_que_quedan} intentos")
 else:
-    print(f"Has agotado el número máximo de intentos. El número a adivinar era {num_secreto}")
-
+    print(f"Has agotado el número máximo de intentos. El número a adivinar era {numero_a_adivinar}")
