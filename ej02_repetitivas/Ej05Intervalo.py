60e26810384ed724f4c0b59a402307ff8853ae16
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2020-11-11 20:21:53 +0100

    Ejercicio 5 de la relación

--- ej02_repetitivas/Ej05Intervalo.py
+++ ej02_repetitivas/Ej05Intervalo.py
@@ -1,14 +1,15 @@
 """
-Escribe un programa que pida el límite inferior y superior de un intervalo.
-Si el límite inferior es mayor que el superior lo tiene que volver a pedir.
-A continuación se van introduciendo números hasta que introduzcamos el 0.
+Propósito: Programa que pide el límite inferior y superior de un intervalo, si el límite inferior es mayor que
+el superior lo tiene que volver a pedir. A continuación se van pidiendo números hasta que se introduzca el 0.
+
 Cuando termine el programa dará las siguientes informaciones:
 * La suma de los números que están dentro del intervalo (intervalo abierto).
 * Cuantos números están fuera del intervalo.
-* E informa si hemos introducido algún número igual a los límites del intervalo.
----
-Análisis
-Pedimos un intervalo (límite inferior y superior)
+* Informa si hemos introducido algún número igual a los límites del intervalo.
+--------------------------------------------------------------------------------
+Análisis:
+--------------------------------------------------------------------------------
+Pedimos un intervalo (límite inferior y superior).
 Nos tenemos que asegurar que el límite inferior sea menor que el superior.
 Se van pidiendo números hasta que se introduzca el 0.
     Si el numero pertenece al intervalo -> lo voy sumando (necesito un acumulador)
@@ -22,17 +23,19 @@ Datos de entrada: límite superior e inferior del intervalo y números.
 Información de salida: La suma de los que pertenecen al intervalo,
 el contador de los números que no pertenecen al intervalo
 indico si un número ha sido igual a algún intervalo
+
 Variables: lim_inf, lim_sup, num, suma_dentro_intervalo, cont_fuera_intervalo (entero),
 igual_limites (Lógico)
----
+----------------------------------------------------------------------------------------
 Diseño:
+----------------------------------------------------------------------------------------
 1.- Inicializo el acumulador, el contador y el interruptor
 2.- Repetir -> leer lim_inf y lim_sup hasta que lim_inf<lim_sup
 3.- Leer num
 4.- Mientras num sea distinto de 0
     5.- Si num pertenece al intervalo -> acumula la suma
     6.- Sino -> incremento contador
-    7.- Si es igual a algún intervalo -> Lo indico (indicador a Verdadero)
+    7.- Si es igual a algún límite del intervalo -> Lo indico (indicador a Verdadero)
     8.- Leer num
 9.- Muestro la suma de los números pertenecientes al intervalo
 10.-Muestro el contador de números no pertenecientes al intervalo
@@ -56,13 +59,13 @@ while lim_inf > lim_sup:
 
 # Proceso
 num = int(input("\nIntroduce un número (0, para salir): "))
-while num!=0:
-    if num>lim_inf and num<lim_sup: # Pertenece al intervalo
+while num != 0:
+    if lim_inf < num < lim_sup:  # Pertenece al intervalo (num>lim_inf and num<lim_sup en otro lenguaje)
         suma_dentro_intervalo += num
-    else:   # No pertenece al intervalo
+    else:  # No pertenece al intervalo
         cont_fuera_intervalo += 1
         # Número igual a alguno de los límites
-        if num==lim_inf or num==lim_sup:
+        if num == lim_inf or num == lim_sup:
             igual_limites = True
     num = int(input("Introduce un número (0, para salir): "))
 
@@ -74,4 +77,3 @@ if igual_limites:
     print("Se ha introducido algún número igual a los límites del intervalo.")
 else:
     print("No se ha introducido ningún número igual a los límites del intervalo.")
-
