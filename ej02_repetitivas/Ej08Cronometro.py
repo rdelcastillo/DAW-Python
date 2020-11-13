a26fce22b6a5feecab1d2ae708445da4252bf444
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2020-11-13 12:32:51 +0100

    Cronómetro

--- ej02_repetitivas/Ej08Cronometro.py
+++ ej02_repetitivas/Ej08Cronometro.py
@@ -14,17 +14,25 @@ segundos = 0
 # Proceso
 # hacemos un ciclo infinito y esperamos 1 segundo cada iteración
 while True:
-    print(f"{horas:02}:{minutos:02}:{segundos:02}", end="")  # 02 para ocupar dos lugares y rellenar con ceros
-    time.sleep(1)
-    # pasar al siguiente segundo
-    if segundos < 59:
-        segundos += 1
-    else:
+    # escribimos el tiempo actual en la pantalla
+    # en el print usamos:
+    #   - 02 para que el tiempo ocupe dos lugares y se rellene con ceros
+    #   - flush=True para forzar a que el buffer de pantalla se escriba
+    print(f"{horas:02}:{minutos:02}:{segundos:02}", end="", flush=True)
+
+    time.sleep(1)   # espera de un segundo
+
+    # pasamos al siguiente segundo
+    segundos += 1
+    # ¿Me he pasado de segundos?
+    if segundos == 60:   # vamos al siguiente minuto
         segundos = 0
-        if minutos < 59:
-            minutos += 1
-        else:
+        minutos += 1
+        # ¿Me he pasado de minutos?
+        if minutos == 60:
             minutos = 0
-            horas += 1
+            horas += 1  # vamos a la hora siguiente
+
     # ponemos el cursor al principio de la línea
     print(8 * "\b", end="")  # \b desplaza el cursor a la izquierda
+
--- ej02_repetitivas/Ej08CronometroV2.py
+++ ej02_repetitivas/Ej08CronometroV2.py
@@ -12,6 +12,6 @@ import time
 for horas in range(0, 24):  # desde las 0 horas a las 23
     for minutos in range(0, 60):  # desde el minuto 0 al 59
         for segundos in range(0, 60):  # desde el segundo 0 al 59
-            print(f"{horas:02}:{minutos:02}:{segundos:02}", end="")
+            print(f"{horas:02}:{minutos:02}:{segundos:02}", end="", flush=True)
             time.sleep(1)  # esperamos un segundo
             print(8 * "\b", end="")  # ponemos el cursor al principio de la línea
