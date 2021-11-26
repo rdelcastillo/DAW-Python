80eec997e35faf9100632f29480a87833738ec4d
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2021-11-26 10:46:30 +0100

    Examen Programación Python primer trismestre curso 2018/19

--- examenes/curso1819trim1/Ejercicio1_cajero.py
+++ examenes/curso1819trim1/Ejercicio1_cajero.py
@@ -1,49 +1,51 @@
-'''
-Cajero de cambio: devuelve y desglosa el cambio en billetes y monedas de forma "ideal"; es decir, con el menor número de billetes y monedas posibles.
+"""
+Cajero de cambio: devuelve y desglosa el cambio en billetes y monedas de forma "ideal"; es decir, con el menor número de
+billetes y monedas posibles.
 
-Pide un valor en euros y devuelve los billetes de 500, 200, 100, 50, 20, 10 y 5 euros, y las monedas de 2€, 1€, 50c, 20c, 10c, 5c, 2c y 1c. Ejemplo:
+Pide un valor en euros y devuelve los billetes de 500, 200, 100, 50, 20, 10 y 5 euros, y las monedas de 2€, 1€, 50c,
+20c, 10c, 5c, 2c y 1c. Ejemplo:
 
 Valor en €uros:  175,50
 
 Cambio: 1 billete de 100€
-		1 billete de 50€
-		2 billetes de 20€
-		1 billete de 5€
-		1 moneda de 50c
-'''
+        1 billete de 50€
+        2 billetes de 20€
+        1 billete de 5€
+        1 moneda de 50c
+"""
 
 # Pedir los euros de los que ha que devolver el cambio
 dinero = input("Dame la cantidad en euros de los que quieres cambio: ")
 
-# Dividir entre euros y centimos
+# Dividir entre euros y céntimos
 euros = int(dinero.split(".")[0])
 cts = int(dinero.split(".")[1])
 
 # Calcular los billetes
-billetes = [500,200,100,50,20,10,5]
+billetes = [500, 200, 100, 50, 20, 10, 5]
 resto = euros
 for billete in billetes:
-    b = resto//billete
-    if b>0:
-        if b==1:
-            print("un billete de",billete,"euros")
+    b = resto // billete
+    if b > 0:
+        if b == 1:
+            print("un billete de", billete, "euros")
         else:
             print(b, "billetes de", billete, "euros")
         resto %= billete
 
 # Calcular monedas
-monedas = [200,100,50,20,10,5,2,1]
-resto = resto*100 + cts
+monedas = [200, 100, 50, 20, 10, 5, 2, 1]
+resto = resto * 100 + cts
 for moneda in monedas:
-    m = resto//moneda
-    if m>0:
-        if moneda>=100:
-            if m==1:
-                print("una moneda de", int(moneda/100), "euros")
+    m = resto // moneda
+    if m > 0:
+        if moneda >= 100:
+            if m == 1:
+                print("una moneda de", int(moneda / 100), "euros")
             else:
-                print(m, "monedas de", int(moneda/100), "euros")
+                print(m, "monedas de", int(moneda / 100), "euros")
         else:
-            if m==1:
+            if m == 1:
                 print("una moneda de", moneda, "centimos de euro")
             else:
                 print(m, "monedas de", moneda, "centimos de euro")
--- examenes/curso1819trim1/Ejercicio2_validaDNI.py
+++ examenes/curso1819trim1/Ejercicio2_validaDNI.py
@@ -1,4 +1,4 @@
-'''
+"""
 Creado el 14 dic. 2018
 
 Pide un DNI y comprueba que es correcto, será correcto si tiene 9 caracteres y la letra es correcta.
@@ -9,42 +9,45 @@ Para calcular la letra se divide el número entre 23 y el resto indica la posici
 Usar una función para validar el DNI y otra que te devuelva la letra del mismo
 
 Autor: Rafael del Castillo
-'''
+"""
+
 
 # ---------
 # Funciones
 # ---------
 
 def dni_correcto(dni):
-    '''
+    """
     Valida el DNI.
-    
+
     El DNI será válido cuando tenga nueve caracteres, los ocho primeros
     sean números y la letra sea correcta.
-    '''
-    
-    return len(dni)==9 and dni[:8].isdecimal() and letra_dni_correcta(dni)
+    """
+
+    return len(dni) == 9 and dni[:8].isdecimal() and letra_dni_correcta(dni)
+
 
 def letra_dni_correcta(dni):
-    '''
+    """
     Valida la letra del DNI pasado como parámetro.
-    
+
     La letra será válida si coincide con la letra resultante de usar el índice del
     resto de la división del número del DNI entre 23 para la cadena de letras:
     "TRWAGMYFPDXBNJZSQVHLCKE"
-    '''
-    
+    """
+
     num_dni = int(dni[:8])
     letra_dni = dni[8].upper()
-    return letra_dni=="TRWAGMYFPDXBNJZSQVHLCKE"[num_dni%23]
+    return letra_dni == "TRWAGMYFPDXBNJZSQVHLCKE"[num_dni % 23]
+
 
 # ---------
 # Principal
 # ---------
 
-if __name__=="__main__":
+if __name__ == "__main__":
     dni = input("Dame un DNI: ")
     if dni_correcto(dni):
-        print("El DNI",dni,"es válido")
+        print("El DNI", dni, "es válido")
     else:
-        print("El DNI",dni,"es incorrecto")
+        print("El DNI", dni, "es incorrecto")
