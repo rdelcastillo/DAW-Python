cde05409bad453e70b629a8de652b75ff557ad65
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2023-02-10 10:32:59 +0100

    Ejercicio de POO: clase de duraciones de tiempo

--- ej05poo/tanda2/ej02duration.py
+++ ej05poo/tanda2/ej02duration.py
@@ -11,6 +11,9 @@ Tiene getters y setters mediante propiedades y métodos para:
 - Sumar y restar segundos, minutos o horas (se cambia el objeto actual).
 - Devolver una cadena con el tiempo almacenado, de forma que si lo que hay es (10 35 5) la cadena sea '10h 35m 5s'
 
+Aunque el ejercicio no lo pide, hemos implementado también la posibilidad de aplicar operaciones relacionales sobre
+los objetos de esta clase sobrecargando los métodos correspondientes.
+
 Autor: Rafael del Castillo Gomariz
 """
 from typeguard import typechecked
@@ -19,7 +22,7 @@ from typeguard import typechecked
 class Duration:
 
     def __init__(self, hours, minutes=None, seconds=None):
-        if isinstance(hours, Duration):
+        if isinstance(hours, Duration) and (minutes, seconds) == (None, None):  # solo tiene que llegar un parámetro
             other = hours
             self.__hours, self.__minutes, self.__seconds = other.hours, other.minutes, other.seconds
         elif isinstance(hours, int) and isinstance(minutes, int) and isinstance(seconds, int):
@@ -34,12 +37,15 @@ class Duration:
         Pasamos todo a segundos, recalculamos las horas, minutos y segundos.
         No admitimos duraciones de tiempo negativas, lanzamos excepción en ese caso.
         """
-        seconds = self.hours * 3600 + self.minutes * 60 + self.seconds
+        seconds = self.__total_seconds()
         if seconds < 0:
             raise ValueError("No puede haber duraciones de tiempo negativas")
         self.__hours = seconds // 3600
         self.__minutes = seconds % 3600 // 60
         self.__seconds = seconds % 3600 % 60
+
+    def __total_seconds(self):
+        return self.hours * 3600 + self.minutes * 60 + self.seconds
 
     @property
     def hours(self):
@@ -92,8 +98,28 @@ class Duration:
     def __repr__(self):
         return f"{self.__class__.__name__}({self.hours}, {self.minutes}, {self.seconds})"
 
+    # Sobrecarga de operadores
+
     def __add__(self, other: 'Duration'):
         return Duration(self.hours + other.hours, self.minutes + other.minutes, self.seconds + other.seconds)
 
     def __sub__(self, other: 'Duration'):
         return Duration(self.hours - other.hours, self.minutes - other.minutes, self.seconds - other.seconds)
+
+    def __eq__(self, other: 'Duration'):
+        return (self.hours, self.minutes, self.seconds) == (other.hours, other.minutes, other.seconds)
+
+    def __ne__(self, other: 'Duration'):
+        return not self == other
+
+    def __lt__(self, other: 'Duration'):
+        return self.__total_seconds() < other.__total_seconds()
+
+    def __le__(self, other: 'Duration'):
+        return self.__total_seconds() <= other.__total_seconds()
+
+    def __gt__(self, other: 'Duration'):
+        return not self <= other
+
+    def __ge__(self, other: 'Duration'):
+        return not self < other
