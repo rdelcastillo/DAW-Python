f1477e5238b7838dfded14007cf9b0aaca1dccfa
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2023-02-15 12:51:04 +0100

    Clase Pila

---
+++ ej05poo/tanda2/ej01Stack.py
@@ -0,0 +1,50 @@
+"""
+Clase que representa una estructura de datos tipo pila (stack).
+
+Permitirá estas operaciones:
+
+- Crear la pila con o sin valores iniciales o a partir de otra pila.
+- Obtener el número de elementos almacenados (tamaño).
+- Saber si está vacía.
+- Vaciarla completamente.
+- Apilar (push): se añade un elemento a la pila. Se añade al principio de esta.
+- Desapilar (pop): se saca (debe devolverse) el elemento superior de la pila y se elimina.
+- Leer el elemento superior de la pila sin retirarlo (top).
+
+Autor: Rafael del Castillo Gomariz
+"""
+from typeguard import typechecked
+
+@typechecked
+class Stack:
+
+    def __init__(self, *values):
+        self.__values = list(values)
+
+    @classmethod
+    def from_stack(cls, other: 'Stack'):
+        new_stack = cls()
+        new_stack.__values = list(other.__values)
+        return new_stack
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(values={self.__values})"
+
+    @property
+    def size(self):
+        return len(self.__values)
+
+    def is_empty(self):
+        return self.size == 0
+
+    def clear(self):
+        self.__values.clear()
+
+    def push(self, value):
+        self.__values.insert(0, value)
+
+    def pop(self):
+        return self.__values.pop(0)
+
+    def top(self):
+        return self.__values[0]
