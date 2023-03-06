1c0a2785ba6eb9cfcfa720ecb7b4cf84a19b4918
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2023-03-06 07:53:34 +0100

    Ejercicio de POO para gestionar una cuenta bancaria

---
+++ ej05poo/tanda3/ej13bankaccount/bank_account.py
@@ -0,0 +1,65 @@
+"""
+Clase BankAccount.
+
+Cada cuenta corriente tiene un número de cuenta de 10 dígitos. El número de cuenta se genera de forma aleatoria cuando
+se crea una cuenta nueva y no puede haber dos objetos con el mismo número de cuenta. La cuenta se puede crear con un
+saldo inicial; en caso de no especificar saldo, se pondrá a cero inicialmente.
+
+En una cuenta se pueden hacer ingresos y gastos. También es posible hacer una transferencia entre una cuenta y otra.
+No se permite el saldo negativo.
+
+Autor: Rafael del Castillo Gomariz.
+"""
+import random
+from typeguard import typechecked
+
+
+@typechecked
+class BankAccount:
+    __registered_accounts = []
+
+    def __init__(self, balance: float = 0):
+        if balance < 0:
+            raise ValueError("No puede crearse una cuenta bancaria con saldo negativo")
+        self.__balance = balance
+        self.__number = BankAccount.__create_number_account()
+
+    @classmethod
+    def __create_number_account(cls):
+        while True:
+            number = random.randint(1, 9999999999)
+            if number not in cls.__registered_accounts:
+                break
+        cls.__registered_accounts.append(number)
+        return number
+
+    @property
+    def balance(self):
+        return self.__balance
+
+    @property
+    def number(self):
+        return self.__number
+
+    def deposit(self, money: float):
+        if money < 0:
+            raise ValueError("Un depósito en cuenta no puede ser negativo")
+        self.__balance += money
+
+    def withdraw(self, money: float):
+        if money < 0:
+            raise ValueError("Un cargo en cuenta no puede ser negativo")
+        if self.__balance - money < 0:
+            raise ValueError("El cargo no se puede hacer porque la cuenta quedaría con saldo negativo")
+        self.__balance -= money
+
+    def transfer(self, other: 'BankAccount', money: float):
+        if money < 0:
+            raise ValueError("Una transferencia no puede ser negativa")
+        if self.__balance - money < 0:
+            raise ValueError("No hay saldo suficiente para hacer la transferencia")
+        self.__balance -= money
+        other.__balance += money
+
+    def __str__(self):
+        return f"Número de cuenta {self.__number:010d} Saldo: {self.__balance:.2f} €"
---
+++ ej05poo/tanda3/ej13bankaccount/bank_account_test.py
@@ -0,0 +1,16 @@
+from bank_account import BankAccount
+
+cuenta1 = BankAccount()
+cuenta2 = BankAccount(1500)
+cuenta3 = BankAccount(6000)
+print(cuenta1)
+print(cuenta2)
+print(cuenta3)
+cuenta1.deposit(2000)
+cuenta2.withdraw(600)
+cuenta3.deposit(75)
+cuenta1.withdraw(55)
+cuenta2.transfer(cuenta3, 100)
+print(cuenta1)
+print(cuenta2)
+print(cuenta3)
