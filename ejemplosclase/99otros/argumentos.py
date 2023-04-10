1c2169d964fcd901c0e9f83502337b230ff615d5
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2023-04-10 08:19:29 +0200

    Ejemplo de paso de argumentos a un programa

--- ejemplosclase/99otros/argumentos.py
+++ ejemplosclase/99otros/argumentos.py
@@ -2,6 +2,11 @@
 
 """
 Ejemplo de paso de argumentos a un script.
+
+La primera línea (#...) es para que este programa pueda funcionar desde un sistema operativo GNU/Linux sin llamar
+explícitamente al intérprete de Python, se hace de forma implícita, y funciona como un script.
+
+Autor: Rafael del Castillo Gomariz.
 """
 
 import sys
@@ -9,5 +14,5 @@ import sys
 print("Nombre programa:", sys.argv[0])
 print("Argumentos pasados:", len(sys.argv)-1)
 print("Argumentos:", sys.argv[1:])
-for n in range(len(sys.argv)):
-    print(f"Argumento {n}: {sys.argv[n]}")
+for n, arg in enumerate(sys.argv):
+    print(f"Argumento {n}: {arg}")
