24154fad010a17b169cbd572fdda784c8e9a88fe
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2023-05-03 09:55:45 +0200

    Ejemplos de creación de fichero XML

---
+++ ejemplosclase/6ficherosxml/ejemplo_escritura.py
@@ -0,0 +1,43 @@
+"""
+Ejemplo de escritura en un fichero XML usando ET.
+
+Añadiremos un nuevo libro a libros.xml
+
+Autor: Rafael del Castillo Gomariz
+"""
+import xml.etree.ElementTree as ET
+
+# Creamos árbol y sacamos la raíz
+XML_FILE = "libros.xml"
+tree = ET.parse(XML_FILE)
+root = tree.getroot()
+
+# Pedimos datos
+print("Adición de libros a la biblioteca")
+print("---------------------------------")
+
+title = input("Título del libro: ")
+genre = input("Género del libro: ").upper()
+author = input("Autor/a del libro: ")
+isbn = input("ISBN del libro: ")
+
+# Creamos el libro y lo ponemos en el árbol xml
+book = ET.Element('book', {'category':genre})
+ET.SubElement(book, 'title').text = title
+ET.SubElement(book, 'author').text = title
+ET.SubElement(book, 'isbn').text = isbn
+root.append(book)
+
+# Escribimos los datos en el fichero
+tree.write(XML_FILE, encoding='unicode')
+
+"""
+La salida del archivo no es "bonita", está sin formatear, podríamos solucionarlo con estas instrucciones:
+
+xml_minidom = minidom.parseString(ET.tostring(root))
+xml_str = xml_minidom.toprettyxml()
+
+# Escribir el XML formateado en un archivo
+with open(XML_FILE, 'w') as archivo:
+    archivo.write(xml_str)
+"""
