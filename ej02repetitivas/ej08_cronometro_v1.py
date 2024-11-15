5f8890f755fb6166c51847d8ad79c65208d1e13c
Rafael del Castillo Gomariz <rafa.castillo@gmail.com>
2024-11-15 09:14:18 +0100

    Modificaciones menores al actualizar la versión

--- ej02repetitivas/ej08_cronometro_v1.py
+++ ej02repetitivas/ej08_cronometro_v1.py
@@ -17,7 +17,7 @@ while True:
     # escribimos el time actual en la pantalla
     # en el print usamos:
     #   - 02 para que el time ocupe dos lugares y se rellene con ceros
-    #   - flush=True para forzar a que el buffer de pantalla se escriba
+    #   - flush=True para forzar a que el buffer de pantalla se escriba (no es necesario)
     print(f"{hours:02d}:{minutes:02d}:{seconds:02d}", end="", flush=True)
 
     time.sleep(1)   # espera de un segundo
--- ej02repetitivas/ej08_cronometro_v2.py
+++ ej02repetitivas/ej08_cronometro_v2.py
@@ -9,9 +9,8 @@ Esta versión funcionará durante 24 horas.
 import time
 
 # Proceso
-for hours in range(0, 24):  # desde las 0 horas a las 23
-    for minutes in range(0, 60):  # desde el minuto 0 al 59
-        for seconds in range(0, 60):  # desde el segundo 0 al 59
-            print(f"{hours:02d}:{minutes:02d}:{seconds:02d}", end="", flush=True)
+for hours in range(24):  # desde las 0 horas a las 23
+    for minutes in range(60):  # desde el minuto 0 al 59
+        for seconds in range(60):  # desde el segundo 0 al 59
+            print(f"\r{hours:02d}:{minutes:02d}:{seconds:02d}", end="")
             time.sleep(1)  # esperamos un segundo
-            print(8 * "\b", end="")  # ponemos el cursor al principio de la línea
